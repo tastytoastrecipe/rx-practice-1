894d95b826c2711108058894c794da2c73c5a0a2
kwon <recipe.muffin@gmail.com>
2022-08-25 18:33:35 +0900

    tableview 추가

--- rx-practice-1/ViewControllers/TableVc.swift
+++ rx-practice-1/ViewControllers/TableVc.swift
@@ -6,24 +6,78 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+import SnapKit
 
 class TableVc: UIViewController {
 
+    let cellId: String = "cell"
+    lazy var table: UITableView = {
+        let tableview = UITableView()
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        return tableview
+    }()
+    
+    lazy var vm: TableVm = {
+        let tablevm = TableVm()
+        return tablevm
+    }()
+    
+    var sectionRelay = BehaviorRelay<[WaterSection]>(value: [])
+    
+    let bag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .brown
-        
+        setConstraints()
+        setContents()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setConstraints() {
+        self.view.addSubview(table)
+        table.snp.makeConstraints { make in make.edges.equalToSuperview() }
     }
-    */
+    
+    func setContents() {
+        sectionRelay.accept(vm.sections)
+        
+        let dataSource = RxTableViewSectionedAnimatedDataSource<WaterSection> { [unowned self] dataSource, tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.cellId, for: indexPath)
+            
+            if #available(iOS 14, *) {
+                var config = cell.defaultContentConfiguration()
+                config.text = item.name
+                config.secondaryText = item.origin
+                cell.contentConfiguration = config
+            } else {
+                cell.textLabel?.text = item.name
+                cell.detailTextLabel?.text = item.origin
+            }
+            return cell
+        }
+        
+        dataSource.canEditRowAtIndexPath = { _, _ in true }
+        
+        sectionRelay
+            .bind(to: table.rx.items(dataSource: dataSource))
+            .disposed(by: bag)
+  
+        /*
+        Observable.just(vm.sections)
+            .bind(to: table.rx.items(dataSource: dataSource))
+            .disposed(by: bag)
+        */
+        
+        table.rx.modelDeleted(Water.self)
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] water in
+                vm.deleteWater(water: water)
+                sectionRelay.accept(vm.sections)
+            })
+            .disposed(by: bag)
+  
+    }
 
 }
