2e205fc74d89055bf8f2236df03a41218802fa9b
kwon <recipe.muffin@gmail.com>
2022-08-27 04:33:58 +0900

    default cell, custom cell 함께 표시

--- rx-practice-1/Models/WaterData.swift
+++ rx-practice-1/Models/WaterData.swift
@@ -5,28 +5,35 @@
 //  Created by orca on 2022/08/25.
 //
 
-import RxDataSources
+//import RxDataSources
+//
+//struct Water: Equatable, IdentifiableType {
+//    var identity: String
+//    var name: String
+//    var origin: String
+//    var company: String
+//}
+//
+//struct WaterSection {
+//    var identity: String
+//    var header: String
+//    var items: [Item]
+//}
+//
+//extension WaterSection: AnimatableSectionModelType {
+//    typealias Item = Water
+//
+//    init(original: WaterSection, items: [Water]) {
+//        self = original
+//        self.items = items
+//    }
+//}
 
-struct Water: Equatable, IdentifiableType {
+struct Water: Equatable {
     var identity: String
     var name: String
     var origin: String
     var company: String
-}
-
-struct WaterSection {
-    var identity: String
-    var header: String
-    var items: [Item]
-}
-
-extension WaterSection: AnimatableSectionModelType {
-    typealias Item = Water
-
-    init(original: WaterSection, items: [Water]) {
-        self = original
-        self.items = items
-    }
 }
 
 
